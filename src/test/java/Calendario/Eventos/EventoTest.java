d4441aaabc4982f57e77d03e2c242ac3fc72f624
melijauregui <mjauregui@fi.uba.ar>
2023-04-23 00:20:07 -0300

    Actualizacion Evento Test

--- src/test/java/Calendario/Eventos/EventoTest.java
+++ src/test/java/Calendario/Eventos/EventoTest.java
@@ -182,4 +182,385 @@ public class EventoTest {
         assertEquals(resEsperadoDescripcion, resObtenidoDescripcion);
     }
 
+    @Test
+    public void TestEventoGetProximoEvento(){
+        //Act
+        var titulo = "LastOfUs";
+        var descripcion = "Capítulos nuevos de la serie The Last of Us";
+        var evento = new Evento();
+        evento.setTitulo(titulo);
+        evento.setDescripcion(descripcion);
+        var repeticion = new RepeticionSemanal(1, List.of(DayOfWeek.SUNDAY), 9);
+
+        var duracion = new Duracion();
+
+        var diaInicio = LocalDate.of(2023, 1, 15);
+        var diaFin = LocalDate.of(2023, 1, 15);
+        var horaInicio = LocalTime.of(22, 00);
+        var horaFin = LocalTime.of(23, 00);
+        duracion.setDiaInicio(diaInicio);
+        duracion.setHoraInicio(horaInicio);
+        duracion.setDiaFin(diaFin);
+        duracion.setHoraFin(horaFin);
+
+        var eventoInicial = new InstanciaEvento();
+        eventoInicial.setDuracion(duracion);
+        evento.setEventoInicial(eventoInicial);
+
+        evento.setRepeticion(repeticion);
+
+        var resEsperadoTitulo = titulo;
+        var resEsperadoDescripcion = descripcion;
+
+        var resEsperadoFechasInicioALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 22, 00),
+                LocalDateTime.of(2023, 1, 22, 22, 00),
+                LocalDateTime.of(2023, 1, 29, 22, 00),
+                LocalDateTime.of(2023, 2, 5, 22, 00),
+                LocalDateTime.of(2023, 2, 12, 22, 00),
+                LocalDateTime.of(2023, 2, 19, 22, 00),
+                LocalDateTime.of(2023, 2, 26, 22, 00),
+                LocalDateTime.of(2023, 3, 5, 22, 00),
+                LocalDateTime.of(2023, 3, 12, 22, 00)
+        );
+        var resEsperadoFechasFinALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 23, 00),
+                LocalDateTime.of(2023, 1, 22, 23, 00),
+                LocalDateTime.of(2023, 1, 29, 23, 00),
+                LocalDateTime.of(2023, 2, 5, 23, 00),
+                LocalDateTime.of(2023, 2, 12, 23, 00),
+                LocalDateTime.of(2023, 2, 19, 23, 00),
+                LocalDateTime.of(2023, 2, 26, 23, 00),
+                LocalDateTime.of(2023, 3, 5, 23, 00),
+                LocalDateTime.of(2023, 3, 12, 23, 00)
+        );
+
+        var resObtenidoTitulo = evento.getTitulo();
+        var resObtenidoDescripcion = evento.getDescripcion();
+
+        //Act
+
+        //Assert
+        for (int i = 0; i < resEsperadoFechasInicioALmacenadas.size(); i++) {
+            var diaFecha = resEsperadoFechasInicioALmacenadas.get(i).minusDays(2);
+            var resObtenido = evento.getProximaRepeticion(diaFecha);
+            assertEquals(resEsperadoFechasInicioALmacenadas.get(i), resObtenido.getFechaInicio());
+            assertEquals(resEsperadoFechasFinALmacenadas.get(i), resObtenido.getFechaFin());
+            assertEquals(resEsperadoTitulo, resObtenido.getTitulo());
+            assertEquals(resEsperadoDescripcion, resObtenido.getDescripcion());
+        }
+        assertEquals(resEsperadoTitulo, resObtenidoTitulo);
+        assertEquals(resEsperadoDescripcion, resObtenidoDescripcion);
+    }
+
+    @Test
+    public void TestEventoGetProximoEventoInfinito(){
+        //Act
+        var titulo = "LastOfUs";
+        var descripcion = "Capítulos nuevos de la serie The Last of Us";
+        var evento = new Evento();
+        evento.setTitulo(titulo);
+        evento.setDescripcion(descripcion);
+        var repeticion = new RepeticionSemanal(1, List.of(DayOfWeek.SUNDAY));
+
+        var duracion = new Duracion();
+
+        var diaInicio = LocalDate.of(2023, 1, 15);
+        var diaFin = LocalDate.of(2023, 1, 15);
+        var horaInicio = LocalTime.of(22, 00);
+        var horaFin = LocalTime.of(23, 00);
+        duracion.setDiaInicio(diaInicio);
+        duracion.setHoraInicio(horaInicio);
+        duracion.setDiaFin(diaFin);
+        duracion.setHoraFin(horaFin);
+
+        var eventoInicial = new InstanciaEvento();
+        eventoInicial.setDuracion(duracion);
+        evento.setEventoInicial(eventoInicial);
+
+        evento.setRepeticion(repeticion);
+
+        var resEsperadoTitulo = titulo;
+        var resEsperadoDescripcion = descripcion;
+
+        var resEsperadoFechasInicioALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 22, 00),
+                LocalDateTime.of(2023, 1, 22, 22, 00),
+                LocalDateTime.of(2023, 1, 29, 22, 00),
+                LocalDateTime.of(2023, 2, 5, 22, 00),
+                LocalDateTime.of(2023, 2, 12, 22, 00),
+                LocalDateTime.of(2023, 2, 19, 22, 00),
+                LocalDateTime.of(2023, 2, 26, 22, 00),
+                LocalDateTime.of(2023, 3, 5, 22, 00),
+                LocalDateTime.of(2023, 3, 12, 22, 00)
+        );
+        var resEsperadoFechasFinALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 23, 00),
+                LocalDateTime.of(2023, 1, 22, 23, 00),
+                LocalDateTime.of(2023, 1, 29, 23, 00),
+                LocalDateTime.of(2023, 2, 5, 23, 00),
+                LocalDateTime.of(2023, 2, 12, 23, 00),
+                LocalDateTime.of(2023, 2, 19, 23, 00),
+                LocalDateTime.of(2023, 2, 26, 23, 00),
+                LocalDateTime.of(2023, 3, 5, 23, 00),
+                LocalDateTime.of(2023, 3, 12, 23, 00)
+        );
+
+        var resObtenidoTitulo = evento.getTitulo();
+        var resObtenidoDescripcion = evento.getDescripcion();
+
+        //Act
+
+        //Assert
+        for (int i = 0; i < resEsperadoFechasInicioALmacenadas.size(); i++) {
+            var diaFecha = resEsperadoFechasInicioALmacenadas.get(i).minusDays(2);
+            var resObtenido = evento.getProximaRepeticion(diaFecha);
+            assertEquals(resEsperadoFechasInicioALmacenadas.get(i), resObtenido.getFechaInicio());
+            assertEquals(resEsperadoFechasFinALmacenadas.get(i), resObtenido.getFechaFin());
+            assertEquals(resEsperadoTitulo, resObtenido.getTitulo());
+            assertEquals(resEsperadoDescripcion, resObtenido.getDescripcion());
+        }
+        assertEquals(resEsperadoTitulo, resObtenidoTitulo);
+        assertEquals(resEsperadoDescripcion, resObtenidoDescripcion);
+    }
+
+    @Test
+    public void TestEventoGetProximoEvento2(){
+        //Act
+        var titulo = "LastOfUs";
+        var descripcion = "Capítulos nuevos de la serie The Last of Us";
+        var evento = new Evento();
+        evento.setTitulo(titulo);
+        evento.setDescripcion(descripcion);
+        var repeticion = new RepeticionSemanal(1, List.of(DayOfWeek.SUNDAY), 9);
+
+        var duracion = new Duracion();
+
+        var diaInicio = LocalDate.of(2023, 1, 15);
+        var diaFin = LocalDate.of(2023, 1, 15);
+        var horaInicio = LocalTime.of(22, 00);
+        var horaFin = LocalTime.of(23, 00);
+        duracion.setDiaInicio(diaInicio);
+        duracion.setHoraInicio(horaInicio);
+        duracion.setDiaFin(diaFin);
+        duracion.setHoraFin(horaFin);
+
+        var eventoInicial = new InstanciaEvento();
+        eventoInicial.setDuracion(duracion);
+        evento.setEventoInicial(eventoInicial);
+
+        evento.setRepeticion(repeticion);
+
+        var resEsperadoTitulo = titulo;
+        var resEsperadoDescripcion = descripcion;
+
+        var resEsperadoFechasInicioALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 22, 00),
+                LocalDateTime.of(2023, 1, 22, 22, 00),
+                LocalDateTime.of(2023, 1, 29, 22, 00),
+                LocalDateTime.of(2023, 2, 5, 22, 00),
+                LocalDateTime.of(2023, 2, 12, 22, 00),
+                LocalDateTime.of(2023, 2, 19, 22, 00),
+                LocalDateTime.of(2023, 2, 26, 22, 00),
+                LocalDateTime.of(2023, 3, 5, 22, 00),
+                LocalDateTime.of(2023, 3, 12, 22, 00)
+        );
+        var resEsperadoFechasFinALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 23, 00),
+                LocalDateTime.of(2023, 1, 22, 23, 00),
+                LocalDateTime.of(2023, 1, 29, 23, 00),
+                LocalDateTime.of(2023, 2, 5, 23, 00),
+                LocalDateTime.of(2023, 2, 12, 23, 00),
+                LocalDateTime.of(2023, 2, 19, 23, 00),
+                LocalDateTime.of(2023, 2, 26, 23, 00),
+                LocalDateTime.of(2023, 3, 5, 23, 00),
+                LocalDateTime.of(2023, 3, 12, 23, 00)
+        );
+
+        var resObtenidoTitulo = evento.getTitulo();
+        var resObtenidoDescripcion = evento.getDescripcion();
+
+        //Act
+
+        //Assert
+        for (int i = 1; i < resEsperadoFechasInicioALmacenadas.size(); i++) {
+            var resObtenido = evento.getProximaRepeticion(eventoInicial);
+            assertEquals(resEsperadoFechasInicioALmacenadas.get(i), resObtenido.getFechaInicio());
+            assertEquals(resEsperadoFechasFinALmacenadas.get(i), resObtenido.getFechaFin());
+            assertEquals(resEsperadoTitulo, resObtenido.getTitulo());
+            assertEquals(resEsperadoDescripcion, resObtenido.getDescripcion());
+            eventoInicial = resObtenido;
+        }
+        assertEquals(resEsperadoTitulo, resObtenidoTitulo);
+        assertEquals(resEsperadoDescripcion, resObtenidoDescripcion);
+    }
+
+    @Test
+    public void TestEventoGetProximoEventoInfinito2(){
+        //Act
+        var titulo = "LastOfUs";
+        var descripcion = "Capítulos nuevos de la serie The Last of Us";
+        var evento = new Evento();
+        evento.setTitulo(titulo);
+        evento.setDescripcion(descripcion);
+        var repeticion = new RepeticionSemanal(1, List.of(DayOfWeek.SUNDAY));
+
+        var duracion = new Duracion();
+
+        var diaInicio = LocalDate.of(2023, 1, 15);
+        var diaFin = LocalDate.of(2023, 1, 15);
+        var horaInicio = LocalTime.of(22, 00);
+        var horaFin = LocalTime.of(23, 00);
+        duracion.setDiaInicio(diaInicio);
+        duracion.setHoraInicio(horaInicio);
+        duracion.setDiaFin(diaFin);
+        duracion.setHoraFin(horaFin);
+
+        var eventoInicial = new InstanciaEvento();
+        eventoInicial.setDuracion(duracion);
+        evento.setEventoInicial(eventoInicial);
+
+        evento.setRepeticion(repeticion);
+
+        var resEsperadoTitulo = titulo;
+        var resEsperadoDescripcion = descripcion;
+
+        var resEsperadoFechasInicioALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 22, 00),
+                LocalDateTime.of(2023, 1, 22, 22, 00),
+                LocalDateTime.of(2023, 1, 29, 22, 00),
+                LocalDateTime.of(2023, 2, 5, 22, 00),
+                LocalDateTime.of(2023, 2, 12, 22, 00),
+                LocalDateTime.of(2023, 2, 19, 22, 00),
+                LocalDateTime.of(2023, 2, 26, 22, 00),
+                LocalDateTime.of(2023, 3, 5, 22, 00),
+                LocalDateTime.of(2023, 3, 12, 22, 00)
+        );
+        var resEsperadoFechasFinALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 23, 00),
+                LocalDateTime.of(2023, 1, 22, 23, 00),
+                LocalDateTime.of(2023, 1, 29, 23, 00),
+                LocalDateTime.of(2023, 2, 5, 23, 00),
+                LocalDateTime.of(2023, 2, 12, 23, 00),
+                LocalDateTime.of(2023, 2, 19, 23, 00),
+                LocalDateTime.of(2023, 2, 26, 23, 00),
+                LocalDateTime.of(2023, 3, 5, 23, 00),
+                LocalDateTime.of(2023, 3, 12, 23, 00)
+        );
+
+        var resObtenidoTitulo = evento.getTitulo();
+        var resObtenidoDescripcion = evento.getDescripcion();
+
+        //Act
+
+        //Assert
+        for (int i = 1; i < resEsperadoFechasInicioALmacenadas.size(); i++) {
+            var resObtenido = evento.getProximaRepeticion(eventoInicial);
+            assertEquals(resEsperadoFechasInicioALmacenadas.get(i), resObtenido.getFechaInicio());
+            assertEquals(resEsperadoFechasFinALmacenadas.get(i), resObtenido.getFechaFin());
+            assertEquals(resEsperadoTitulo, resObtenido.getTitulo());
+            assertEquals(resEsperadoDescripcion, resObtenido.getDescripcion());
+            eventoInicial = resObtenido;
+        }
+        assertEquals(resEsperadoTitulo, resObtenidoTitulo);
+        assertEquals(resEsperadoDescripcion, resObtenidoDescripcion);
+    }
+
+    @Test
+    public void TestEventoSetsAnsGettersCambiarDiaInicio(){
+        //Act
+        var titulo = "LastOfUs";
+        var descripcion = "Capítulos nuevos de la serie The Last of Us";
+        var evento = new Evento();
+        evento.setTitulo(titulo);
+        evento.setDescripcion(descripcion);
+
+        var duracion = new Duracion();
+
+        var diaInicio = LocalDate.of(2023, 1, 15);
+        var diaFin = LocalDate.of(2023, 1, 15);
+        var horaInicio = LocalTime.of(22, 00);
+        var horaFin = LocalTime.of(23, 00);
+        duracion.setDiaInicio(diaInicio);
+        duracion.setHoraInicio(horaInicio);
+        duracion.setDiaFin(diaFin);
+        duracion.setHoraFin(horaFin);
+
+        var eventoInicial = new InstanciaEvento();
+        eventoInicial.setDuracion(duracion);
+
+        evento.setEventoInicial(eventoInicial);
+
+        List<InstanciaEvento> resEsperadoEventosIntervalo = new ArrayList<>();
+        resEsperadoEventosIntervalo.add(eventoInicial);
+
+        var repeticion2 = new RepeticionSemanal(1, List.of(DayOfWeek.SUNDAY), 9);
+        evento.setRepeticion(repeticion2);
+
+
+        var resEsperadoTitulo = titulo;
+        var resEsperadoDescripcion = descripcion;
+
+        var resEsperadoFechasInicioALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 22, 00),
+                LocalDateTime.of(2023, 1, 22, 22, 00),
+                LocalDateTime.of(2023, 1, 29, 22, 00),
+                LocalDateTime.of(2023, 2, 5, 22, 00),
+                LocalDateTime.of(2023, 2, 12, 22, 00),
+                LocalDateTime.of(2023, 2, 19, 22, 00),
+                LocalDateTime.of(2023, 2, 26, 22, 00),
+                LocalDateTime.of(2023, 3, 5, 22, 00),
+                LocalDateTime.of(2023, 3, 12, 22, 00)
+        );
+        var resEsperadoFechasFinALmacenadas = List.of(
+                LocalDateTime.of(2023, 1, 15, 23, 00),
+                LocalDateTime.of(2023, 1, 22, 23, 00),
+                LocalDateTime.of(2023, 1, 29, 23, 00),
+                LocalDateTime.of(2023, 2, 5, 23, 00),
+                LocalDateTime.of(2023, 2, 12, 23, 00),
+                LocalDateTime.of(2023, 2, 19, 23, 00),
+                LocalDateTime.of(2023, 2, 26, 23, 00),
+                LocalDateTime.of(2023, 3, 5, 23, 00),
+                LocalDateTime.of(2023, 3, 12, 23, 00)
+        );
+
+        var resObtenidoTitulo = evento.getTitulo();
+        var resObtenidoDescripcion = evento.getDescripcion();
+
+        //Act
+        var resObtenidoEventosIntervalo2 = evento.getAlmacenamientoFechas();
+
+        //Assert
+        assertEquals(resEsperadoFechasInicioALmacenadas.size(), resObtenidoEventosIntervalo2.size());
+        for (int i = 0; i < resEsperadoFechasInicioALmacenadas.size(); i++) {
+            assertEquals(resEsperadoFechasInicioALmacenadas.get(i), resObtenidoEventosIntervalo2.get(i).getFechaInicio());
+            assertEquals(resEsperadoFechasFinALmacenadas.get(i), resObtenidoEventosIntervalo2.get(i).getFechaFin());
+        }
+        assertEquals(resEsperadoTitulo, resObtenidoTitulo);
+        assertEquals(resEsperadoDescripcion, resObtenidoDescripcion);
+        
+        //--------------------------------------------------------------------
+        var duracion2 = new Duracion();
+        var diaInicio2 = LocalDate.of(2023, 1, 17);
+        var diaFin2 = LocalDate.of(2023, 1, 17);
+        var horaInicio2 = LocalTime.of(22, 00);
+        var horaFin2 = LocalTime.of(23, 00);
+        duracion2.setDiaInicio(diaInicio2);
+        duracion2.setHoraInicio(horaInicio2);
+        duracion2.setDiaFin(diaFin2);
+        duracion2.setHoraFin(horaFin2);
+
+        var eventoInicial2 = new InstanciaEvento();
+        eventoInicial2.setDuracion(duracion2);
+
+        //Assert
+        assertEquals(resEsperadoFechasInicioALmacenadas.size(), resObtenidoEventosIntervalo2.size());
+        for (int i = 1; i < resEsperadoFechasInicioALmacenadas.size(); i++) {
+            assertEquals(resEsperadoFechasInicioALmacenadas.get(i), resObtenidoEventosIntervalo2.get(i).getFechaInicio());
+            assertEquals(resEsperadoFechasFinALmacenadas.get(i), resObtenidoEventosIntervalo2.get(i).getFechaFin());
+        }
+        assertEquals(resEsperadoTitulo, resObtenidoTitulo);
+        assertEquals(resEsperadoDescripcion, resObtenidoDescripcion);
+    }
+
 }
