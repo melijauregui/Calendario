d4e9ac3792686f122b1c9fb0d948fddaa4b56a94
melijauregui <mjauregui@fi.uba.ar>
2023-06-04 15:26:59 -0300

    unFondo

--- src/main/java/MVC/Vista.java
+++ src/main/java/MVC/Vista.java
@@ -2,25 +2,18 @@ package MVC;
 
 import Calendario.Main.Calendario;
 import javafx.collections.FXCollections;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
-
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 
@@ -28,19 +21,11 @@ public class Vista {
     private Calendario calendario;
     private Stage stage;
     private Scene scene;
-    private Label diaActual;
+    private LocalDate diaActual;
     private ChoiceBox<String> choiceFrecuencia = crearChoiceBox();
-    //private ChoiceBox<String> choiceDia = crearChoiceBox();
-    //private ChoiceBox<String> choiceSemana = crearChoiceBox();
-    //private ChoiceBox<String> choiceMes = crearChoiceBox();
     private AnchorPane paneGeneral = new AnchorPane();
     private AnchorPane actualFondo;
     private String frecuencia;
-
-    //private AnchorPane semanaFondo = new AnchorPane();
-    //private AnchorPane diaFondo = new AnchorPane();
-    //private AnchorPane mesFondo = new AnchorPane();
-
 
     public Vista(Calendario calendario, Stage stage) throws IOException {
         this.calendario = calendario;
@@ -57,14 +42,9 @@ public class Vista {
     }
 
     private void initialize(){
-        /*diaFondo.getChildren().add(choiceDia);
-        semanaFondo.getChildren().add(choiceSemana);
-        mesFondo.getChildren().add(choiceMes);*/
-
         paneGeneral.getChildren().add(choiceFrecuencia);
 
-        diaActual = new Label(LocalDate.now().toString());
-        //diaActual.
+        diaActual = LocalDate.now();
     }
     private ChoiceBox crearChoiceBox(){
         ChoiceBox box = new ChoiceBox<>(FXCollections.observableArrayList("Dia","Semana", "Mes"));
@@ -81,27 +61,6 @@ public class Vista {
     public String getEscuchaFrecuencia() {
         return choiceFrecuencia.getValue();
     }
-
-    /*
-    public void registrarEscuchaMes(EventHandler<ActionEvent> eventHandler) {
-        choiceMes.setOnAction(eventHandler);
-    }
-    public void registrarEscuchaDia(EventHandler<ActionEvent> eventHandler) {
-        choiceDia.setOnAction(eventHandler);
-    }
-    public void registrarEscuchaSemana(EventHandler<ActionEvent> eventHandler) {
-        choiceSemana.setOnAction(eventHandler);
-    }
-
-    public String getEscuchaMes() {
-        return choiceMes.getValue();
-    }
-    public String getEscuchaDia() {
-        return choiceDia.getValue();
-    }
-    public String getEscuchaSemana() {
-        return choiceSemana.getValue();
-    }*/
 
     public void tipoRango(String opcion) {
         if (opcion.equals("Semana")) {
@@ -125,15 +84,18 @@ public class Vista {
     private void setearDia(){
         setearPane("file:src/main/java/MVC/imagenes/diario.png");
         frecuencia = "Dia";
+        setearFechas();
 
     }
     private void setearMes(){
         setearPane("file:src/main/java/MVC/imagenes/mensual.png");
         frecuencia = "Mes";
+        setearFechas();
     }
     private void setearSemana(){
         setearPane("file:src/main/java/MVC/imagenes/semanal.png");
         frecuencia = "Semana";
+        setearFechas();
     }
 
     private void setearFondo(AnchorPane pane, String ruta) {
@@ -143,6 +105,39 @@ public class Vista {
         pane.getChildren().add(imageView);
     }
 
+    private void setearFechas(){
+        if (frecuencia == "Semana"){
+            setearFechasSemana();
+        }else if (frecuencia == "Mes"){
+            setearFechasMes();
+        }
+    }
 
+    private void setearFechasSemana(){
 
+    }
+    private void setearFechasMes(){
+        var mesActual = diaActual.getMonth();
+        LocalDate primerDia = LocalDate.of(diaActual.getYear(), mesActual, 1);
+        while (primerDia.getDayOfWeek() != DayOfWeek.MONDAY){
+            primerDia = primerDia.minusDays(1);
+        }
+        int columna = 0;
+        int fila = 0;
+        double x = 110;
+        double y = 140;
+        while (primerDia.getMonth() == mesActual || primerDia.getMonth() == mesActual.minus(1)) {
+            Label fecha = new Label(Integer.toString(primerDia.getDayOfMonth()));
+            fecha.setLayoutX(x+105.5*columna);
+            fecha.setLayoutY(y+47*fila);
+            actualFondo.getChildren().add(fecha);
+            primerDia = primerDia.plusDays(1);
+            if (columna == 6){
+                fila++;
+                columna = 0;
+            }else{
+                columna++;
+            }
+        }
+    }
 }
